dfca8ac68d69e533b954462094890faf0e723891
Matthew Bentley <matthew.t.bentley@gmail.com>
2015-11-09 21:30:55 -0500

    Fix bug where page stops updating by forcing it to reload after a minute of no activity

--- autopoke.py
+++ autopoke.py
@@ -15,14 +15,20 @@ if __name__ == '__main__':
     assert "Forgot password?" not in driver.page_source
 
     c = 0
+    c2 = 0
     while True:
         try:
             for i in driver.find_elements_by_link_text("Poke Back"):
                 i.click()
                 c += 1
+                c2 = 0
                 print("Clicked so far: " + str(c))
         except StaleElementReferenceException:
             print("Found exception, reloading page")
             driver.get('https://facebook.com/pokes/')
 
+        c2 += 1
+        if c2 % 121 == 0:
+            print("No pokes in last minute.  Reloading")
+            driver.get('https://facebook.com/pokes/')
         sleep(0.5)
