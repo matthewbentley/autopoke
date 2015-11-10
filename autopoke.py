746eace7e4677b034743b25e0f8d53aabd07dd5c
Matthew Bentley <matthew.t.bentley@gmail.com>
2015-11-10 18:14:49 -0500

    Fix bugs?

--- autopoke.py
+++ autopoke.py
@@ -15,20 +15,20 @@ if __name__ == '__main__':
     assert "Forgot password?" not in driver.page_source
 
     c = 0
-    c2 = 0
+    e = 0
     while True:
         try:
             for i in driver.find_elements_by_link_text("Poke Back"):
                 i.click()
                 c += 1
-                c2 = 0
                 print("Clicked so far: " + str(c))
         except StaleElementReferenceException:
-            print("Found exception, reloading page")
-            driver.get('https://facebook.com/pokes/')
+            e += 1
+            if e == 10:
+                print("Found exception, reloading page")
+                driver.get('https://facebook.com/pokes/')
+                e = 0
+            else:
+                print("Found exception, doing nothing")
 
-        c2 += 1
-        if c2 % 121 == 0:
-            print("No pokes in last minute.  Reloading")
-            driver.get('https://facebook.com/pokes/')
-        sleep(0.5)
+        sleep(1)
